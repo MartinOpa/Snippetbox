db11f52a0873fefa960c803d146b82ad51f8d15c
MartinOpa <martin.op@icloud.com>
2022-12-26 01:26:49 +0100

    dynamic HTML templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	//"html/template"
-
-	//"log"
 	"net/http"
 	"strconv"
 
@@ -23,30 +20,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-	/*
-		files := []string{
-			"./ui/html/home.page.tmpl",
-			"./ui/html/base.layout.tmpl",
-			"./ui/html/footer.partial.tmpl",
-		}
-
-		ts, err := template.ParseFiles(files...)
-		if err != nil {
-			//app.errorLog.Println(err.Error())
-			app.serverError(w, err) //http.Error(w, "Internal Server Error", 500)
-			return
-		}
-
-		err = ts.Execute(w, nil)
-		if err != nil {
-			//app.errorLog.Println(err.Error())
-			app.serverError(w, err) //http.Error(w, "Internal Server Error", 500)
-		}*/
-
-	//w.Write([]byte("Hello from Snippetbox"))
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -64,7 +40,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
